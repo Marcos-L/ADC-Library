46430f350ce30d6294e348d1585162cd2c754f11
Marcos-L <mloaizaa@unal.edu.co>
2021-03-16 14:31:38 -0500

    Initial Commit

---
+++ Core/Inc/ADClib.h
@@ -0,0 +1,14 @@
+/*
+ * ADClib.h
+ *
+ *  Created on: Mar 16, 2021
+ *      Author: Marco
+ */
+
+#ifndef INC_ADCLIB_H_
+#define INC_ADCLIB_H_
+
+void ADC_Pin_Config(void);
+int AnalogRead(ADC_HandleTypeDef);
+
+#endif /* INC_ADCLIB_H_ */
